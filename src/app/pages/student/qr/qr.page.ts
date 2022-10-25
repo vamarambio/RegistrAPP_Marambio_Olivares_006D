21331271451f39b034910ae2dee9efdff560e43f
vamarambio <v.marambio@duocuc.cl>
2022-10-25 20:11:11 -0300

    Implemented QR code scanner functions

--- src/app/pages/student/qr/qr.page.ts
+++ src/app/pages/student/qr/qr.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 import { MenuController } from '@ionic/angular';
 
 @Component({
@@ -6,7 +7,10 @@ import { MenuController } from '@ionic/angular';
   templateUrl: './qr.page.html',
   styleUrls: ['./qr.page.scss'],
 })
-export class QrPage implements OnInit {
+export class QrPage implements OnInit, OnDestroy {
+
+  scannedResult: any;
+  content_visibility = '';
 
   constructor(private menuController: MenuController) { }
 
@@ -17,4 +21,52 @@ export class QrPage implements OnInit {
     this.menuController.open('first');
   }
 
+  async checkPermission() {
+    try {
+      // check or request permission
+      const status = await BarcodeScanner.checkPermission({ force: true });
+      if (status.granted) {
+        // the user granted permission
+        return true;
+      }
+      return false;
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  async startScan() {
+    try {
+      const permission = await this.checkPermission();
+      if(!permission) {
+        return;
+      }
+      await BarcodeScanner.hideBackground();
+      document.querySelector('body').classList.add('scanner-active');
+      this.content_visibility = 'hidden';
+      const result = await BarcodeScanner.startScan();
+      console.log(result);
+      BarcodeScanner.showBackground();
+      document.querySelector('body').classList.remove('scanner-active');
+      this.content_visibility = '';
+      if(result?.hasContent) {
+        this.scannedResult = result.content;
+        console.log(this.scannedResult);
+      }
+    } catch(e) {
+      console.log(e);
+      this.stopScan();
+    }
+  }
+
+  stopScan() {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+    document.querySelector('body').classList.remove('scanner-active');
+    this.content_visibility = '';
+  }
+
+  ngOnDestroy(): void {
+    this.stopScan();
+  }
 }
